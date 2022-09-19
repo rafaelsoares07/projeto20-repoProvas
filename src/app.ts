da10c57aee1f8734d7b493f1dcf4036cb3f8f1ff
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-09-19 12:54:28 -0300

    criando rota get de teste

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,11 @@ app.use(cors())
 app.use(express.json())
 
 app.use(indexRouter) //Chama todas as rotas da nossa API
+
+app.get("/get",(req,res)=>{
+    res.status(200).send("rota get funfando")
+})
+
 app.use(errorHandleMiddleware)// Precisa ser chamado depois de todas as rotas!
 
 export default app
